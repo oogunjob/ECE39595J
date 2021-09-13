b21cc36e4b50e454710f5e73321755e01dcbe1f2
Tosin Ogunjobi <54332223+oogunjob@users.noreply.github.com>
2021-09-12 20:20:24 -0400

    Create Vehicle.java

---
+++ HW3/oogunjob/vehicle/Vehicle.java
@@ -0,0 +1,37 @@
+class Vehicle {
+
+   private String model;
+
+   Vehicle(String _model) {
+      model = _model;
+      System.out.print(model);
+   }
+
+   String getModel( ) { // RINK MAKE PRIVATE
+      return model;
+   }
+
+   String getVehicleModel( ) {
+      return getModel( );
+   }
+
+   String getColor( ) { 
+      return null;
+   }
+
+   String getEngine( ) {
+      return null;
+   }
+
+   String getTransmission( ) {
+      return null;
+   }
+
+   int getNumPassengers( ) {
+      return 0;
+   }
+
+   String getWarranty( ) {
+      return null;
+   }
+}
