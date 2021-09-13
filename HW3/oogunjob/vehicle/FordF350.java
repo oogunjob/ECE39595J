fbed51516975dc7f3abba2c20daf761c71dad8fe
Tosin Ogunjobi <54332223+oogunjob@users.noreply.github.com>
2021-09-12 20:19:36 -0400

    Create FordF350.java

---
+++ HW3/oogunjob/vehicle/FordF350.java
@@ -0,0 +1,34 @@
+class FordF350 extends Vehicle {
+
+   private String color;
+
+   FordF350(String _color) {
+      super("Ford F350");
+      color = _color;
+      System.out.println(" built");
+   }
+
+   String getModel( ) { // RINK MAKE PRIVATE
+      return model;
+   }
+
+   String getColor( ) { // RINK MAKE PRIVATE
+      return color;
+   }
+
+   String getEngine( ) {
+      return "7.3L V8";
+   }
+
+   String getTransmission( ) {
+      return "10 speed automatic";
+   }
+
+   int getNumPassengers( ) {
+      return 3;
+   }
+
+   String getWarranty( ) {
+      return "3/36,000 + 5/60,000 Powertrain";
+   } 
+}
